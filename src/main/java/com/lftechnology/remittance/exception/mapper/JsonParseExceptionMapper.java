759b492a1fa2fff249c31a3a81171998fa07491b
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2017-03-16 11:29:25 +0545

    Add exception mapper for bad json format

---
+++ src/main/java/com/lftechnology/remittance/exception/mapper/JsonParseExceptionMapper.java
@@ -0,0 +1,24 @@
+package com.lftechnology.remittance.exception.mapper;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.lftechnology.remittance.pojo.ErrorMessage;
+
+/**
+ * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
+ */
+@Provider
+public class JsonParseExceptionMapper implements ExceptionMapper<JsonParseException> {
+
+    @Override
+    public Response toResponse(JsonParseException exception) {
+        ErrorMessage errorMessage = new ErrorMessage();
+        errorMessage.setMessage("Invalid request body format");
+
+        return Response.status(Response.Status.BAD_REQUEST).entity(errorMessage).type(MediaType.APPLICATION_JSON).build();
+    }
+}
