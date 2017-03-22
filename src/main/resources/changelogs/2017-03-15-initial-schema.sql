5d0586c798a3794c66aed76d532cf2f4b13acec4
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2017-03-22 16:33:26 +0545

    create table if not exists

--- src/main/resources/changelogs/2017-03-15-initial-schema.sql
+++ src/main/resources/changelogs/2017-03-15-initial-schema.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset achyut:initial-schema-design
-create table users (
+CREATE TABLE IF NOT EXISTS users (
     id uuid primary key,
     name varchar(255),
     address varchar(255)
