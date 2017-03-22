013f72eb61bb12b125c4b0748c1a84c3956b451c
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2017-03-22 16:32:54 +0545

    reformat

--- src/main/java/com/lftechnology/remittance/dao/impl/BaseDao.java
+++ src/main/java/com/lftechnology/remittance/dao/impl/BaseDao.java
@@ -21,97 +21,97 @@ import com.lftechnology.remittance.exception.DataAccessException;
  */
 public abstract class BaseDao<T, Pk> implements CrudDao<T, Pk> {
 
-	@Inject
-	protected EntityManager em;
+    @Inject
+    protected EntityManager em;
 
-	protected Class<T> entityClass;
+    protected Class<T> entityClass;
 
-	public BaseDao(Class<T> entityClass) {
-		this.entityClass = entityClass;
-	}
+    public BaseDao(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.lftechnology.remittance.dao.CrudDao#save(java.lang.Object)
-	 */
-	@Override
-	public T save(T entity) {
-		try {
-			em.persist(entity);
-		} catch (PersistenceException persistenceException) {
-			throw new DataAccessException(persistenceException.getMessage());
-		}
-		return entity;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.lftechnology.remittance.dao.CrudDao#save(java.lang.Object)
+     */
+    @Override
+    public T save(T entity) {
+        try {
+            em.persist(entity);
+        } catch (PersistenceException persistenceException) {
+            throw new DataAccessException(persistenceException.getMessage());
+        }
+        return entity;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.lftechnology.remittance.dao.CrudDao#update(java.lang.Object)
-	 */
-	@Override
-	public T update(T entity) {
-		try {
-			entity = em.merge(entity);
-		} catch (PersistenceException persistenceException) {
-			throw new DataAccessException(persistenceException.getMessage());
-		}
-		return entity;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.lftechnology.remittance.dao.CrudDao#update(java.lang.Object)
+     */
+    @Override
+    public T update(T entity) {
+        try {
+            entity = em.merge(entity);
+        } catch (PersistenceException persistenceException) {
+            throw new DataAccessException(persistenceException.getMessage());
+        }
+        return entity;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.lftechnology.remittance.dao.CrudDao#remove(java.lang.Object)
-	 */
-	@Override
-	public void remove(T entity) {
-		try {
-			em.remove(entity);
-		} catch (PersistenceException persistenceException) {
-			throw new DataAccessException(persistenceException.getMessage());
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.lftechnology.remittance.dao.CrudDao#remove(java.lang.Object)
+     */
+    @Override
+    public void remove(T entity) {
+        try {
+            em.remove(entity);
+        } catch (PersistenceException persistenceException) {
+            throw new DataAccessException(persistenceException.getMessage());
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.lftechnology.remittance.dao.CrudDao#removeById(java.lang.Object)
-	 */
-	@Override
-	public void removeById(Pk id) {
-		try {
-			T entity = em.getReference(entityClass, id);
-			em.remove(entity);
-		} catch (PersistenceException persistenceException) {
-			throw new DataAccessException(persistenceException.getMessage());
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.lftechnology.remittance.dao.CrudDao#removeById(java.lang.Object)
+     */
+    @Override
+    public void removeById(Pk id) {
+        try {
+            T entity = em.getReference(entityClass, id);
+            em.remove(entity);
+        } catch (PersistenceException persistenceException) {
+            throw new DataAccessException(persistenceException.getMessage());
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.lftechnology.remittance.dao.CrudDao#findById(java.lang.Object)
-	 */
-	@Override
-	public T findById(Pk id) {
-		return em.find(entityClass, id);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.lftechnology.remittance.dao.CrudDao#findById(java.lang.Object)
+     */
+    @Override
+    public T findById(Pk id) {
+        return em.find(entityClass, id);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.lftechnology.remittance.dao.CrudDao#findByFilter()
-	 */
-	@Override
-	public List<T> findByFilter() {
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-		CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(entityClass);
-		Root<T> from = criteriaQuery.from(entityClass);
-		CriteriaQuery<T> select = criteriaQuery.select(from);
-		TypedQuery<T> typedQuery = em.createQuery(select);
-		return typedQuery.getResultList();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.lftechnology.remittance.dao.CrudDao#findByFilter()
+     */
+    @Override
+    public List<T> findByFilter() {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(entityClass);
+        Root<T> from = criteriaQuery.from(entityClass);
+        CriteriaQuery<T> select = criteriaQuery.select(from);
+        TypedQuery<T> typedQuery = em.createQuery(select);
+        return typedQuery.getResultList();
+    }
 
 }
